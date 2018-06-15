df5ec00b154b4f4b23dbfc2bdfd1385a76bd078c
Javier Ruitti <ruittijavier@gmail.com>
2018-06-15 11:38:24 -0300

    Ejercicio para Iteracion 3

--- Exam/src/test/CalificacionUnitTest.java
+++ Exam/src/test/CalificacionUnitTest.java
@@ -7,14 +7,15 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
 import model.Calificacion;
-import model.Examen;
 import model.Estudiante;
+import model.Examen;
 
 class CalificacionUnitTest {
 
 	@Test
 	void getResultado_NotaDiez_RetornaSobresaliente() {
-		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III", "Ingeniería en Sistemas");
+		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III",
+				"Ingeniería en Sistemas");
 		Estudiante unEstudiante = new Estudiante("123456", "Diaz", "Pedro", "Av. San Martín 25", "La Rioja");
 		Calificacion unaCalificacion = new Calificacion(unEstudiante, unExamen, 10);
 
@@ -25,8 +26,35 @@ class CalificacionUnitTest {
 	}
 
 	@Test
+	void getResultado_NotaUnoACuatro_RetornaReprobado() {
+		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III",
+				"Ingeniería en Sistemas");
+		Estudiante unEstudiante = new Estudiante("123456", "Diaz", "Pedro", "Av. San Martín 25", "La Rioja");
+		Calificacion unaCalificacion = new Calificacion(unEstudiante, unExamen, 3);
+
+		String resultado = unaCalificacion.getResultado();
+
+		assertEquals("REPROBADO", resultado);
+
+	}
+
+	@Test
+	void getResultado_NotaCincoASiete_RetornaAprobado() {
+		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III",
+				"Ingeniería en Sistemas");
+		Estudiante unEstudiante = new Estudiante("123456", "Diaz", "Pedro", "Av. San Martín 25", "La Rioja");
+		Calificacion unaCalificacion = new Calificacion(unEstudiante, unExamen, 6);
+
+		String resultado = unaCalificacion.getResultado();
+
+		assertEquals("APROBADO", resultado);
+
+	}
+
+	@Test
 	void getResultado_NotaSuperiorADiez_DevuelveError() {
-		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III", "Ingeniería en Sistemas");
+		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III",
+				"Ingeniería en Sistemas");
 		Estudiante unEstudiante = new Estudiante("123456", "Diaz", "Pedro", "Av. San Martín 25", "La Rioja");
 		Calificacion unaCalificacion = new Calificacion(unEstudiante, unExamen, 15);
 
@@ -35,10 +63,11 @@ class CalificacionUnitTest {
 		assertEquals("ERROR", resultado);
 
 	}
-	
+
 	@Test
 	void getResultado_NotaInferiorAUno_DevuelveError() {
-		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III", "Ingeniería en Sistemas");
+		Examen unExamen = new Examen(LocalDateTime.of(2018, 6, 13, 16, 0), "Programación III",
+				"Ingeniería en Sistemas");
 		Estudiante unEstudiante = new Estudiante("123456", "Diaz", "Pedro", "Av. San Martín 25", "La Rioja");
 		Calificacion unaCalificacion = new Calificacion(unEstudiante, unExamen, 0);
 
@@ -47,7 +76,5 @@ class CalificacionUnitTest {
 		assertEquals("ERROR", resultado);
 
 	}
-	
-
 
 }
